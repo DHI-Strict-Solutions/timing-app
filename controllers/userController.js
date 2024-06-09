e4aa26bcb52cc23117c2b17969046693d49f3ea9
DRdarwin <31813699+DRdarwin@users.noreply.github.com>
2024-06-09 08:23:58 +0300

    Add files via upload
    Signed-off-by: DRdarwin <31813699+DRdarwin@users.noreply.github.com>

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,45 +1,57 @@
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
 
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
-exports.createUser = async (req, res) => {
+  exports.registerUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const { name, email, password, role } = req.body;
 
-  try {
-    let user = await User.findOne({ email });
-
+ try {
+    // Check if user already exists
+    let user = await User.findOne({ email: email.toLowerCase() });
     if (user) {
       return res.status(400).json({ msg: 'User already exists' });
     }
 
     user = new User({
       name,
-      email,
+      email: email.toLowerCase(),
       password,
       role
     });
 
+    // Encrypt password
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
 
     await user.save();
 
-    res.json({ msg: 'User registered successfully' });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-};
+    // Return jsonwebtoken
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
 
-// @route    GET api/users
-// @desc     Get all users
-// @access   Private
-exports.getUsers = async (req, res) => {
-  try {
-    const users = await User.find().select('-password');
-    res.json(users);
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
